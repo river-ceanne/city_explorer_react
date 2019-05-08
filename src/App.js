5546bacd15e2273d05b2974718718ea4f2adcc58
river-ceanne <snvencer@gmail.com>
2019-05-08 10:14:59 -0700

    saving progress..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,110 @@
 import React from 'react';
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-    
-    </div>
+    <header>
+      <h1>City Explorer</h1>
+      <p>Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!</p>
+    </header>
   );
+};
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: [],
+    };
+  }
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <Search />
+        <Map />
+        <Result />
+        <Result />
+        <Result />
+        <Result />
+        <Result />
+      </React.Fragment>
+    );
+  }
+}
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search_query: '',
+    };
+  }
+
+  handleSearchKeyword = e => {
+    e.preventDefault();
+    this.setState({ search_query });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    let data = await superagent.get('https://swapi.co/api/people/');
+    let apiResults = data.body.results.reduce((list, person) => {
+      list[person.name] = person.url;
+      return list;
+    }, {});
+
+    this.props.handler(apiResults);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleSearchKeyword} />
+        <button onClick={handleSubmit}>Search</button>
+      </form>
+    );
+  }
+}
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class="mapouter"><div class="gmap_canvas">
+      <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=seattel&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+      </iframe><a href="https://www.crocothemes.net"></a></div>
+      <style>.mapouter{position:relative;text-align:right;height:500px;width:600px;} .gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style>
+      </div>
+    );
+  }
+}
+
+class Result extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <section>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </section>
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Main />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
